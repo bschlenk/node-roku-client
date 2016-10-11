37c9edd953c883499aaa2eff666909459d1c3382
sgnl <rayrfarias@gmail.com>
2016-10-11 10:43:17 -1000

    updates tests with wrapper helper function

--- test/nodeku.spec.js
+++ test/nodeku.spec.js
@@ -2,19 +2,30 @@ const Test = require('tape')
 const Nodeku = require('../')
 const Mocks = require('./mocks')
 
+function wrapper(description, fn) {
+  Test(description, t => {
+    Nodeku(null, Mocks.Client)
+    .then(device => {
+      fn(t, device);
+    })
+    .catch(t.fail)
+  })
+}
+
 Test('Nodeku initialization', t => {
   t.ok(typeof Nodeku === 'function', 'is ready')
   t.end()
 })
 
-Test('Nodeku Discovery', t => {
-  t.timeoutAfter(12000)
+wrapper('Nodeku Discovery', (t, device) => {
+  t.timeoutAfter(2000)
 
-  Nodeku(null, Mocks.Client)
-    .then(device => {
-      t.assert(typeof device === 'object', 'module received')
-      t.assert(device.ip() === '192.168.1.17:8060', 'device has an ip address')
-      t.end()
-    })
-    .catch(t.fail)
+  t.assert(typeof device === 'object', 'module received')
+  t.assert(device.ip() === '192.168.1.17:8060', 'device has an ip address')
+
+  t.end()
+})
+
+wrapper('Nodeku Device Methods', (t, device) => {
+
 })
