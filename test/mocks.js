ddb8990faff18b3a17a36f420a493507d2797c05
sgnl <rayrfarias@gmail.com>
2016-10-11 10:33:42 -1000

    removes timeout from mock because "F.I.R.S.T"

--- test/mocks.js
+++ test/mocks.js
@@ -15,18 +15,14 @@ class Client {
   }
 
   on(eventName, callback) {
-    let randomNumber = Math.floor(Math.random() * 10000);
-    setTimeout(_ => {
       callback({
         'CACHE-CONTROL': 'max-age=3600',
         ST: 'urn:dial-multiscreen-org:service:dial:1',
         USN: 'uuid:00000000-0000-0000-0000-000000000000::urn:dial-multiscreen-org:service:dial:1',
         EXT: '',
         SERVER: 'Roku UPnP/1.0 MiniUPnPd/1.4',
-        LOCATION: 'http://192.168.1.17:8060/dial/dd.xml',
-        _timeoutNumber: randomNumber
+        LOCATION: 'http://192.168.1.17:8060/dial/dd.xml'
       })
-    },  randomNumber)
   }
 }
 
