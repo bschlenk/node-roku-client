e35f5702385342cdbcac6f46eb81bf88e9eb1f4f
sgnl <rayrfarias@gmail.com>
2016-10-10 23:03:13 -1000

    updates tests: roku device discovery

--- test/nodeku.spec.js
+++ test/nodeku.spec.js
@@ -1,6 +1,18 @@
 const Test = require('tape')
 const Nodeku = require('../')
+const Mocks = require('./mocks')
 
-Test('Nodeku exists', t => {
-  t.assert(typeof Nodeku === 'function', 'is a Function.')
+Test('Nodeku initialization', t => {
+  t.ok(typeof Nodeku === 'function', 'is ready.')
+  t.end()
+})
+
+Test('Nodeku Discovery', t => {
+  t.plan(1)
+
+  Nodeku(null, Mocks.Client)
+    .then(deviceAddr => {
+      t.assert(deviceAddr === '192.168.1.17:8060')
+    })
+    .catch(t.fail)
 })
