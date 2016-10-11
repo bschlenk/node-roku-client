1eea84facd9769130d3082e7d5ad90e1a7fffbdc
sgnl <rayrfarias@gmail.com>
2016-10-11 10:31:35 -1000

    updates code format

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const NodeSSDPClient = require('node-ssdp').Client
 const Device = require('./lib/device')
 
-module.exports = function(timeout, CustomClient) {
+module.exports = (timeout, CustomClient) => {
 
   timeout = timeout || 10000
 
@@ -10,20 +10,8 @@ module.exports = function(timeout, CustomClient) {
 
     let RokuUrl
 
+    // CustomClient is used for testing at the moment.
     let Client = new CustomClient || new NodeSSDPClient()
-
-    Client.on('response', function inResponse(headers, code, rinfo) {
-      let ipAddress = /(\d+.*:8060)(?=\/)/.exec(headers.LOCATION)
-
-      if (!!~headers.SERVER.search(/Roku/) && ipAddress) {
-        RokuUrl = ipAddress[0]
-
-        clearInterval(IntervalId)
-        clearTimeout(TimeoutId)
-
-        return resolve(Device(RokuUrl))
-      }
-    })
 
     const IntervalId = setInterval(_ => {
       Client.search('ssdp:all')
@@ -36,5 +24,19 @@ module.exports = function(timeout, CustomClient) {
 
       return reject(new Error(`Could not find any Roku devices. Time spent: ${timeout / 1000} seconds`))
     }, timeout)
+
+    Client.on('response', function inResponse(headers/*, code, rinfo*/) {
+      let ipAddress = /(\d+.*:8060)(?=\/)/.exec(headers.LOCATION)
+
+      if (!!~headers.SERVER.search(/Roku/) && ipAddress) {
+        RokuUrl = ipAddress[0]
+
+        clearInterval(IntervalId)
+        clearTimeout(TimeoutId)
+
+        return resolve(Device(RokuUrl))
+      }
+    })
+
   })
 }
