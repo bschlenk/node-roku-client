d03dbfbc2498f4beffac2ca29a43a1205a9f43e4
sgnl <rayrfarias@gmail.com>
2016-10-11 10:54:32 -1000

    re-arrange test order

--- test/nodeku.passing.spec.js
+++ test/nodeku.passing.spec.js
@@ -17,18 +17,18 @@ Test('initialization', t => {
   t.end()
 })
 
+wrapper('methods: .ip()', (t, device) => {
+  t.ok(device.hasOwnProperty('ip'), 'exists')
+  t.ok(typeof device.ip === 'function' , 'is a function')
+  t.assert(device.ip() === '192.168.1.17:8060', 'ip address retreived')
+  t.end()
+})
+
 wrapper('discovery', (t, device) => {
   t.timeoutAfter(2000)
 
   t.assert(typeof device === 'object', 'module received')
   t.assert(device.ip() === '192.168.1.17:8060', 'ip address located')
 
-  t.end()
-})
-
-wrapper('methods: .ip()', (t, device) => {
-  t.ok(device.hasOwnProperty('ip'), 'exists')
-  t.ok(typeof device.ip === 'function' , 'is a function')
-  t.assert(device.ip() === '192.168.1.17:8060', 'ip address retreived')
   t.end()
 })
