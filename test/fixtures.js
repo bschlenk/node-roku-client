5048e06b5debe526ccc79efc81da4bde3b50ca41
sgnl <rayrfarias@gmail.com>
2016-10-12 00:49:17 -1000

    adds /query/icon/:appId route mock

--- test/fixtures.js
+++ test/fixtures.js
@@ -4,6 +4,7 @@ const XmlFiles = [
   { name: 'AppsXML', location: './helpers/apps.xml' },
   { name: 'ActiveAppXML', location: './helpers/active-app.xml' },
   { name: 'InfoXML', location: './helpers/info.xml' },
+  { name: 'NetflixIcon', location: './helpers/netflix.jpeg' },
 ]
 
 module.exports = XmlFiles.reduce((module, file) => {
