2a47009290712496e8223093c020ec53a3f6ef5a
sgnl <rayrfarias@gmail.com>
2016-10-17 15:19:57 -1000

    forces discovery test to run serially

--- test/discovery.test.js
+++ test/discovery.test.js
@@ -8,12 +8,12 @@ Discovery = Discovery.bind({
   MockSSDPClient: SsdpMock.Client
 })
 
-Test('discovery exists and returns a Promise', t => {
+Test.serial('discovery exists and returns a Promise', t => {
   t.true(Discovery instanceof Function, 'is a Function')
   t.true(Discovery() instanceof Promise, 'returns a Promise')
 })
 
-Test('discovery returns device module when a Roku device is found', t => {
+Test.serial('discovery returns device module when a Roku device is found', t => {
   return Discovery()
     .then(device => {
       t.is(typeof device, 'object', 'is a module')
