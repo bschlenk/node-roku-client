9564e81b89389160a6ac597fe5b2edd2336c678e
Brian Schlenker <bschlenk@umich.edu>
2020-12-09 22:45:37 -0500

    feat: remove deprecated exports
    BREAKING CHANGE:
    
    These deprecated exports are no longer available when importing from the
    package. Use the alternatives instead:
    
    - keys -> Keys
    - Client -> RokuClient

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -1,11 +1,9 @@
-import { RokuClient, Keys, Client, keys, discover, discoverAll } from '..';
+import { RokuClient, Keys, discover, discoverAll } from '..';
 
 describe('index', () => {
   it('should export the expected interfaces', () => {
     expect(RokuClient).toBeDefined();
     expect(Keys).toBeDefined();
-    expect(Client).toBeDefined();
-    expect(keys).toBeDefined();
     expect(discover).toBeDefined();
     expect(discoverAll).toBeDefined();
   });
--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,6 @@
-import * as Keys from './keys';
 import { RokuClient } from './client';
 
 export default RokuClient;
 export * from './client';
 export * from './discover';
-
-/**
- * Import `RokuClient` instead.
- * @deprecated
- */
-const Client = RokuClient;
-
-/**
- * Import `Keys` instead.
- * @deprecated
- */
-const keys = Keys;
-
-export { Keys, Client, keys };
+export * as Keys from './keys';
