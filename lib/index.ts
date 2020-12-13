10c88b9adb8a91c650f4685008c2ce6bbca0f577
Brian Schlenker <bschlenk@umich.edu>
2020-12-13 17:04:56 -0500

    fix: export RokuDeviceInfo interface

--- lib/index.ts
+++ lib/index.ts
@@ -3,4 +3,5 @@ import { RokuClient } from './client';
 export default RokuClient;
 export * from './client';
 export * from './discover';
+export * from './device-info';
 export * as Keys from './keys';
