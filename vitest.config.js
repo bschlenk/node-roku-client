f8bbc4aee9e8dc170dc02c085ec2cfb4ad9e20b3
Brian Schlenker <bschlenk@umich.edu>
2025-04-07 23:15:29 -0400

    chore: setup coverage and update github test workflow for pnpm

--- vitest.config.js
+++ vitest.config.js
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     setupFiles: ['./lib/setupTests.js'],
-  }
+    coverage: {
+      enabled: true,
+      include: ['lib/**/*.ts'],
+      exclude: ['lib/setupTests.ts'],
+    },
+  },
 })
